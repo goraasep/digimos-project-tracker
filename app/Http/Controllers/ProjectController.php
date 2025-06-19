69c201d4c643d0781f5247ac41ac325def5cbdc7
goraasep <gora.asep@gmail.com>
2025-06-19 15:37:12 +0700

    fix: date input

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -96,8 +96,8 @@ class ProjectController extends Controller
             $validated = $request->validate([
                 'title' => 'required',
                 'client' => 'required',
-                'start_date' => 'required',
-                'end_date' => 'required',
+                'start_date' => 'required|date',
+                'end_date' => 'required|date|after_or_equal:start_date',
                 'description' => 'nullable|string',
             ]);
             $validated['created_by'] = Auth::user()->id;
@@ -135,8 +135,8 @@ class ProjectController extends Controller
             $validated = $request->validate([
                 'title' => 'required',
                 'client' => 'required',
-                'start_date' => 'required',
-                'end_date' => 'required',
+                'start_date' => 'required|date',
+                'end_date' => 'required|date|after_or_equal:start_date',
                 'description' => 'nullable|string',
             ]);
             Project::where('id', $id)->update($validated);
